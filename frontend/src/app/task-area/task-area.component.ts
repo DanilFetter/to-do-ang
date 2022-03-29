7d03b600bfc7d088aedf888abcd4c9cc02327eef
DanilVezmenov <dvezminov.exceedteam@gmail.com>
2022-03-29 16:46:18 +0300

    feat(tasks): display first-pass

--- frontend/src/app/task-area/task-area.component.ts
+++ frontend/src/app/task-area/task-area.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import TaskInterface from '../interface';
+import { TaskLoaderService } from '../task-loader.service';
 @Component({
   selector: 'app-task-area',
   templateUrl: './task-area.component.html',
-  styleUrls: ['./task-area.component.scss']
+  styleUrls: ['./task-area.component.scss'],
 })
 export class TaskAreaComponent implements OnInit {
-
-  constructor() { }
+  DisplayedTask: TaskInterface[] = [];
+  constructor(private readonly taskLoaderService: TaskLoaderService) {}
 
   ngOnInit(): void {
+    this.DisplayedTask = this.taskLoaderService.getItems();
   }
-
 }
