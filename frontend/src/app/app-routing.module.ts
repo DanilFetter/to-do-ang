4b83ca605de9eda4f4d6284b378f6eb972f86fba
DanilVezmenov <dvezminov.exceedteam@gmail.com>
2022-03-29 13:46:17 +0300

    feat(layout): layout first pass

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TaskAreaComponent } from './task-area/task-area.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "**", component: TaskAreaComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
