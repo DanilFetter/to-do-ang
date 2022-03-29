3840b3920bddcc33b23d5adef5be8894222d446b
DanilVezmenov <dvezminov.exceedteam@gmail.com>
2022-03-29 13:46:17 +0300

    layout created

---
+++ frontend/src/app/task-area/task-area.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-task-area',
+  templateUrl: './task-area.component.html',
+  styleUrls: ['./task-area.component.scss']
+})
+export class TaskAreaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
