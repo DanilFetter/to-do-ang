5748b3360e3d814d14b312cfe850215b25405d3f
DanilVezmenov <dvezminov.exceedteam@gmail.com>
2022-03-31 17:26:13 +0300

    fix: removed unused styles and files, fix empty lines

--- frontend/src/app/task-loader.service.ts
+++ frontend/src/app/task-loader.service.ts
@@ -1,19 +1,20 @@
 import { Injectable } from '@angular/core';
+
 export interface TaskInterface {
   id: string;
   text: string;
   status: boolean;
-  tabGroupe:string;
+  tabGroupe: string;
 }
+
 @Injectable({
   providedIn: 'root',
 })
-
 export class TaskLoaderService {
   private dataStore: TaskInterface[] = [
-    { id: '1', text: 'Feed cat', status: false, tabGroupe: "Uncategorized" },
-    { id: '1', text: 'Feed cat', status: true ,tabGroupe: "College"},
-    { id: '1', text: 'Feed cat', status: false,tabGroupe: "Payments"},
+    { id: '1', text: 'Feed cat', status: false, tabGroupe: 'Uncategorized' },
+    { id: '1', text: 'Feed cat', status: true, tabGroupe: 'College' },
+    { id: '1', text: 'Feed cat', status: false, tabGroupe: 'Payments' },
   ];
 
   getItems(): TaskInterface[] {
