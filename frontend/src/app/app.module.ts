f4782535af25d0d24f1a7f1b3fbd7c6fc878be50
Danil <dvezminov.exceedteam@gmail.com>
2022-04-05 17:05:50 +0300

    feat: task area split on independence components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TaskAreaComponent } from './task-area/task-area.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainPageComponent } from './main-page/main-page.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TaskAreaComponent,
+    MainPageComponent,
+    SideBarComponent,
   ],
   imports: [
     BrowserModule,
