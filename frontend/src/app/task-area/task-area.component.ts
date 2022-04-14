a2d09d60a9acce7bbb170a745c6a0aa7f4222ef0
Danil Feter <danyafetter.work@gmail.com>
2022-04-14 12:43:02 +0300

    feat:(task.area) added FormControl functional, displaying invalid/valid form status

--- frontend/src/app/task-area/task-area.component.ts
+++ frontend/src/app/task-area/task-area.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { TaskLoaderService, TaskInterface } from '../task-loader.service';
+
 @Component({
   selector: 'app-task-area',
   templateUrl: './task-area.component.html',
@@ -10,6 +12,8 @@ export class TaskAreaComponent implements OnInit {
   displayedTasks: TaskInterface[] = [];
   currenTab: string = 'All';
 
+  taskText = new FormControl('', [Validators.required, Validators.minLength(4)]);
+
   ngOnInit(): void {
     this.displayedTasks = this.taskLoaderService.getItems();
   }
@@ -18,18 +22,21 @@ export class TaskAreaComponent implements OnInit {
     this.displayedTasks = this.taskLoaderService.changeTaskStatus(id, status);
   }
 
-  createTask(event: KeyboardEvent) {
-    if (event.key === 'Enter') {
-      const target = event.target as HTMLInputElement;
+  createTask() {
+    const taskText = this.taskText.value;
+    if(this.taskText.valid){
+      this.taskText.setValue('');
       this.displayedTasks = this.taskLoaderService.createNewTask(
-        target.value,
+        taskText,
         this.currenTab
       );
-      target.value = '';
     }
+
   }
 
   deleteTask(id: string) {
     this.displayedTasks = this.taskLoaderService.removeTask(id);
   }
 }
+
+
